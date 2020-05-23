c1cbbd7d98f6cc360494eb3099e48cc801754779
eric soucy <eric.soucy@gmail.com>
2020-05-23 15:39:20 -0400

    better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,20 +6,4 @@ import { UsersServiceService } from "./users-service.service";
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  constructor(private usersService: UsersServiceService) {}
-
-  /*
-  onSetToInactive(id: number) {
-    this.usersService.addInactiveUser(id);
-
-    //this.inactiveUsers.push(this.activeUsers[id]);
-    //this.activeUsers.splice(id, 1);
-  }
-
-  onSetToActive(id: number) {
-    this.usersService.addActiveUser(id);
-    //this.activeUsers.push(this.inactiveUsers[id]);
-    //this.inactiveUsers.splice(id, 1);
-  }*/
-}
+export class AppComponent {}
